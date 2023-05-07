1a5fe80ad447928c1fc41e2727f27177452b2b1e
Denis Paskin <paskin.denis.00@gmail.com>
2023-05-07 18:07:37 +0200

    implemented Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { logo } from '../assets';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz-logo" className="w-28 object-contain" />
+        <button
+          className="black_btn"
+          type="button"
+          onClick={() =>
+            window.open('https://github.com/dzianisPaskin/GPT_AI_WEBSITE')
+          }
+        >
+          GitHub
+        </button>
+      </nav>
 
-export default Hero
+      <h1 className="head_text">
+        Summarize Articles width <br className='max-md:hidden'/>
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+          Simplify your reading with Summize, an open-source article summarizer
+          that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
